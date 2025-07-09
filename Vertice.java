07357931ac42d659fb342fa2d04e7307243bf198
LauS-V <lvalderrama8602@gmail.com>
2025-07-08 22:44:46 -0500

    Update Vertice.java

--- Vertice.java
+++ Vertice.java
@@ -1,1 +1,11 @@
+public class Vertice {
+    int valor;
+    Vertice siguiente_i;
+    Vertice siguiente_p;
 
+    Vertice(int valor) {
+        this.valor = valor;
+        this.siguiente_i = null;
+        this.siguiente_p = null;
+    }
+}
